c0f1b9702165e51d479e746c54b24ae508d69dea
Saniya Khandelwal <readytofire17@gmail.com>
2026-07-03 19:30:29 +0530

    Implement program to find largest and second largest elements

---
+++ sLargestElement.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  
+  int arr[n] ;
+  for(int i = 0; i < n; i++)
+{
+    cin >> arr[i];
+}
+  int largest = arr[0];
+  int slargest = -1;
+  
+  for(int i =1; i<n; i++){
+      if (arr[i] > largest){
+          slargest = largest;
+          largest = arr[i];
+      }
+      else if(arr[i] > slargest && arr[i] != largest){
+          slargest = arr[i];
+      }
+  }
+cout << slargest << endl;
+cout << largest<< endl;
+    return 0;
+}
